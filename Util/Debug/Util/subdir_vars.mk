5742809968d96994664e1eac2e12aac74eba2399
TipsyCape <jmhubka@gmail.com>
2022-09-12 19:19:15 -0500

    util

---
+++ Util/Debug/Util/subdir_vars.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Util/launchpad.c \
+../Util/led.c \
+../Util/pushbutton.c \
+../Util/uart.c 
+
+C_DEPS += \
+./Util/launchpad.d \
+./Util/led.d \
+./Util/pushbutton.d \
+./Util/uart.d 
+
+OBJS += \
+./Util/launchpad.obj \
+./Util/led.obj \
+./Util/pushbutton.obj \
+./Util/uart.obj 
+
+OBJS__QUOTED += \
+"Util\launchpad.obj" \
+"Util\led.obj" \
+"Util\pushbutton.obj" \
+"Util\uart.obj" 
+
+C_DEPS__QUOTED += \
+"Util\launchpad.d" \
+"Util\led.d" \
+"Util\pushbutton.d" \
+"Util\uart.d" 
+
+C_SRCS__QUOTED += \
+"../Util/launchpad.c" \
+"../Util/led.c" \
+"../Util/pushbutton.c" \
+"../Util/uart.c" 
+
+
